298f0b4b209aa7456a47c5aa726f0e6d97cc279b
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2019-12-13 06:12:42 -0300

    Modelo dos dados da app orders

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    product = models.CharField(max_length=255)
+    quantidade = models.IntegerField()
+    suppliers = models.ForeignKey('suppliers.Suppliers', on_delete=models.PROTECT)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product
