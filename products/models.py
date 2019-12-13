4c08f669c1418795354365757ab4f6fd81a93062
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2019-12-13 06:35:10 -0300

    Modelo da app products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    suppliers = models.ForeignKey('suppliers.Suppliers', on_delete=models.PROTECT)
+    amount = models.IntegerField()
+    buy = models.DecimalField(max_digits=6, decimal_places=2)
+    sale = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
