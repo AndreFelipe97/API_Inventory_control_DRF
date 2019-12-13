5bc35d28d2efd7a7f63a5e12ab53efc5cdcbd1d6
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2019-12-12 21:53:19 -0300

    Foi alterado o nome do campo razao para o padrão em ingles

--- fornecedores/models.py
+++ fornecedores/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Fornecedor(models.Model):
     cod = models.IntegerField()
-    razao = models.CharField(max_length=255)
+    corporate_name = models.CharField(max_length=255)
     cnpj = models.CharField(max_length=11)
     fone = models.CharField(max_length=11)
     public_place = models.CharField(max_length=100)
