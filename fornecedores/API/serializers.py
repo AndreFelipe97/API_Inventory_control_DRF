c6db875c10b07a8c59aa9a32dbd001ae1e3c6089
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2019-12-12 22:11:43 -0300

    Foi colocado no padrão ingles

--- fornecedores/API/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from fornecedores.models import Fornecedor
-
-class FornecedorSerializer(ModelSerializer):
-    class Meta:
-        model = Fornecedor
-        fields = ['cod', 'corporate_name', 'cnpj']
--- fornecedores/API/viewsets.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from fornecedores.models import Fornecedor
-from .serializers import FornecedorSerializer
-
-class FornecedorViewSet(ModelViewSet):
-    queryset = Fornecedor.objects.all()
-    serializer_class = FornecedorSerializer
