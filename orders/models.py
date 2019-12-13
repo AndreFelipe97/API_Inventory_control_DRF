4d418a0ec318b6b603b45742efeb8fc9bd2c24a4
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2019-12-13 06:26:43 -0300

    Foi criado o modelo de order

--- orders/models.py
+++ orders/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Order(models.Model):
     product = models.CharField(max_length=255)
-    quantidade = models.IntegerField()
+    amount = models.IntegerField()
     suppliers = models.ForeignKey('suppliers.Suppliers', on_delete=models.PROTECT)
     date = models.DateTimeField(auto_now=True)
 
