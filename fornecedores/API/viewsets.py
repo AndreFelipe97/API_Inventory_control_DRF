826d205d1c6c8d4a886dd1126077f9d02092c951
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2019-12-12 22:01:23 -0300

    Foi renomeado o arquivo serializer

--- fornecedores/API/viewsets.py
+++ fornecedores/API/viewsets.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from fornecedores.models import Fornecedor
-from .serializer import FornecedorSerializer
+from .serializers import FornecedorSerializer
 
 class FornecedorViewSet(ModelViewSet):
-    queryset = Fornecedor
+    queryset = Fornecedor.objects.all()
     serializer_class = FornecedorSerializer
